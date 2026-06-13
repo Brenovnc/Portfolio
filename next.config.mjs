67b56e0da6746c8c32e8854a5a36ff74c045838f
Brenovnc <brenovnc435@gmail.com>
2026-06-13 00:24:39 -0300

    Deploy github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/Portfolio' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/Portfolio/' : '',
   typescript: {
     ignoreBuildErrors: true,
   },
